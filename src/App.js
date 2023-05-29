4f326ef42b20a994cb050f44ca6875a4c184431f
Lewis ushindi <coderflame3@gmail.com>
2023-05-29 15:31:54 +0300

    Display selected image in UploadForm component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import UploadForm from './components/UploadForm';
 import CompressionSettings from './components/CompressionSettings';
 import CompressButton from './components/CompressButton';
 
+
 function App() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [compressionSettings, setCompressionSettings] = useState({
@@ -25,35 +26,40 @@ function App() {
 
   return (
     <div className="container min-h-screen bg-white">
-        <div class="flex items-center p-4 justify-between shadow-lg mb-4">
-          <h1 class="text-2xl font-bold mr-2">OptiSnap</h1>
-          <a href="https://github.com/Frenziecodes/optisnap" target="_blank">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32.58 31.77" height="30">
-              <path d="M16.29,0A16.29,16.29,0,0,0,.42,17.54c0,7.29,4.72,13.49,11.28,15.67.82.15,1.11-.36,1.11-.79,0-.39,0-1.42,0-2.79-4.59,1-5.56-2.21-5.56-2.21A4.39,4.39,0,0,0,4.2,25.17c-1.51-.84.11-.82.11-.82a3.47,3.47,0,0,1,2.53,1.71,3.48,3.48,0,0,0,4.77,1.36,3.49,3.49,0,0,1,1-2.19c-3.6-.41-7.4-1.8-7.4-8a6.28,6.28,0,0,1,1.68-4.34,5.81,5.81,0,0,1,.16-4.29s1.36-.43,4.45,1.66a15.3,15.3,0,0,1,8.12,0C22.77,6.43,24.13,7.86,24.13,7.86a5.8,5.8,0,0,1,.16,4.29,6.27,6.27,0,0,1,1.68,4.34c0,6.23-3.81,7.58-7.44,7.98a3.9,3.9,0,0,1,1.12,3,.79.79,0,0,0,.09.32c0,.38.09,1.4,0,2.79,0,.43.29.94,1.12.79A16.29,16.29,0,0,0,16.29,0Z"/>
-            </svg>
-          </a>
+      <div className="flex items-center p-4 justify-between shadow-lg mb-4">
+        <h1 className="text-2xl font-bold mr-2">OptiSnap</h1>
+        <a href="https://github.com/Frenziecodes/optisnap" target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 32.58 31.77"
+            height="30"
+          >
+            <path d="M16.29,0A16.29,16.29,0,0,0,.42,17.54c0,7.29,4.72,13.49,11.28,15.67.82.15,1.11-.36,1.11-.79,0-.39,0-1.42,0-2.79-4.59,1-5.56-2.21-5.56-2.21A4.39,4.39,0,0,0,4.2,25.17c-1.51-.84.11-.82.11-.82a3.47,3.47,0,0,1,2.53,1.71,3.48,3.48,0,0,0,4.77,1.36,3.49,3.49,0,0,1,1-2.19c-3.6-.41-7.4-1.8-7.4-8a6.28,6.28,0,0,1,1.68-4.34,5.81,5.81,0,0,1,.16-4.29s1.36-.43,4.45,1.66a15.3,15.3,0,0,1,8.12,0C22.77,6.43,24.13,7.86,24.13,7.86a5.8,5.8,0,0,1,.16,4.29,6.27,6.27,0,0,1,1.68,4.34c0,6.23-3.81,7.58-7.44,7.98a3.9,3.9,0,0,1,1.12,3,.79.79,0,0,0,.09.32c0,.38.09,1.4,0,2.79,0,.43.29.94,1.12.79A16.29,16.29,0,0,0,16.29,0Z" />
+          </svg>
+        </a>
+      </div>
+
+      <div className="flex mb-3">
+        <div className="p-4 w-full">
+          <UploadForm onFileSelect={handleFileSelect} selectedFile={selectedFile} />
+          <div className="h-56 shadow-2xl p-4 pt-6 mt-4">
+            <CompressionSettings
+              settings={compressionSettings}
+              onChange={handleCompressionSettingsChange}
+            />
+            <CompressButton onClick={handleCompress} />
+          </div>
         </div>
 
-
-        <div className='flex mb-3'>
-
-          <div className='p-4 w-full'>                   
-            <UploadForm onFileSelect={handleFileSelect} />
-            <div className='h-56 shadow-2xl p-4 pt-6 mt-4'>
-              <CompressionSettings
-                settings={compressionSettings}
-                onChange={handleCompressionSettingsChange}
-              />
-              <CompressButton onClick={handleCompress} />
-            </div>          
+        <div className="bg-white w-full p-4">
+          <div className="w-40 h-40 bg-gray-200 border border-gray-400">
+            {/* Placeholder div for displaying the compressed image */}
           </div>
-          
-          <div className='bg-white w-full p-4'>           
-            {/* compressed picture will be displayed  here and also a download button to download the image here */}
-          </div>    
-
-        </div>   
-
+          <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
+            Download
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/UploadForm.jsx
+++ src/components/UploadForm.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function UploadForm({ onFileSelect }) {
+function UploadForm({ onFileSelect, selectedFile }) {
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     onFileSelect(file);
   };
 
   return (
-    <div className="shadow-xl p-3 h-56">
+    <div className="shadow-xl p-3 h-68">
       <h2 className="text-lg font-bold mb-2">Select an Image to Compress:</h2>
       <input
         type="file"
@@ -15,8 +15,15 @@ function UploadForm({ onFileSelect }) {
         onChange={handleFileChange}
         className="mb-4"
       />
-      <div className="w-40 h-40 bg-gray-200 border border-gray-400"></div>
-      {/* Placeholder div for displaying the chosen image */}
+      <div className="w-40 h-40 bg-gray-200 border border-gray-400">
+        {selectedFile && (
+          <img
+            src={URL.createObjectURL(selectedFile)}
+            alt="Selected"
+            className="w-full h-full object-cover"
+          />
+        )}
+      </div>
     </div>
   );
 }
